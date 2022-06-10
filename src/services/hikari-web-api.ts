67ed81b45b19503d222585b3fa622809a6089774
SDMasterGames <sdmastergames@hotmail.com>
2022-06-09 20:31:16 -0400

    arquivo para acessar a api do site da hikari

---
+++ src/services/hikari-web-api.ts
@@ -0,0 +1,14 @@
+import "dotenv/config"
+import axios from "axios";
+
+const baseURL = process.env["HIKARI_WEB_API_URL"];
+
+if (!baseURL) {
+  throw new Error("HIKARI_WEB_API_URL is not defined");
+}
+
+const HikariWebApi = axios.create({
+  baseURL,
+});
+
+export { HikariWebApi };
