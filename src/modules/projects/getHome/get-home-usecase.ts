2dd898d00f1522b8525f3a685e9de98b1f3c87fb
SDMasterGames <sdmastergames@hotmail.com>
2022-06-12 18:20:34 -0400

    adicionando a logica de cache

--- src/modules/projects/getHome/get-home-usecase.ts
+++ src/modules/projects/getHome/get-home-usecase.ts
@@ -1,18 +1,28 @@
 import { ProjectsError } from "../../../repositories/errors";
+import { ICacheRepository } from "../../../repositories/interface-cache-repository";
 import { IProjectsRepository } from "../../../repositories/interface-projects-repository";
 import { IHttpResponse, ok, serverError } from "../../httpHelper";
 import { IGetHomeRequestDTO } from "./get-home-dto";
 
 export class getHomeUseCase {
-  constructor(private IProjectsRepository: IProjectsRepository) {}
+  constructor(
+    private IProjectsRepository: IProjectsRepository,
+    private ICacheRepository: ICacheRepository
+  ) {}
 
   async execute({ page }: IGetHomeRequestDTO): Promise<IHttpResponse> {
     try {
+      const cacheKey = `home:${page}`;
+
+      const cache = await this.ICacheRepository.get(cacheKey);
+      if (cache) return ok(cache);
+
       const response = await this.IProjectsRepository.getProjectsReleases(page);
 
       if (response instanceof ProjectsError)
         return serverError(response.message);
 
+      await this.ICacheRepository.set(cacheKey, response, 20);
       return ok(response);
     } catch (error: any) {
       return serverError(error.message);
--- src/modules/projects/getHome/index.ts
+++ src/modules/projects/getHome/index.ts
@@ -1,10 +1,12 @@
+import { CacheRepository } from "../../../repositories/implements/cache-repository";
 import { ProjectsRepository } from "../../../repositories/implements/projects-repository";
 import { getHomeController } from "./get-home-controller";
 import { getHomeUseCase } from "./get-home-usecase";
 
 const projectsRepository = new ProjectsRepository();
+const cacheRepository = new CacheRepository();
 
-const GetHomeUseCase = new getHomeUseCase(projectsRepository);
+const GetHomeUseCase = new getHomeUseCase(projectsRepository, cacheRepository);
 
 const GetHomeController = new getHomeController(GetHomeUseCase);
 
