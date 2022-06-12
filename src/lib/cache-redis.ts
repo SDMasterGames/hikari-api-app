db2cdff44063817a0b118c2702ee57ca8f7d49de
SDMasterGames <sdmastergames@hotmail.com>
2022-06-12 18:21:19 -0400

    conexão com o redis

---
+++ src/lib/cache-redis.ts
@@ -0,0 +1,32 @@
+import Redis from "ioredis";
+
+let redis: Redis;
+export function connectionRedis(): Redis | null {
+  try {
+    const host = process.env["REDIS_HOST"];
+    const port = process.env["REDIS_PORT"];
+    const password = process.env["REDIS_PASSWORD"];
+
+    if (!host || !port || !password) {
+      console.error("missing redis config");
+      return null;
+    }
+
+    if (redis) {
+      return redis;
+    }
+    redis = new Redis({
+      host,
+      port: Number(port),
+      password,
+    });
+
+    redis.on("error", (err) => {
+      redis.disconnect();
+      console.error(err);
+    });
+    return redis;
+  } catch (error) {
+    return null;
+  }
+}
