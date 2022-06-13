17ed5693bbcbf278e2ac7596c551ae69a4b5ef89
SDMasterGames <sdmastergames@hotmail.com>
2022-06-13 16:55:53 -0400

    exportação do usecase e controller

---
+++ src/modules/projects/getChapters/index.ts
@@ -0,0 +1,17 @@
+import { CacheRepository } from "../../../repositories/implements/cache-repository";
+import { ProjectsRepository } from "../../../repositories/implements/projects-repository";
+import { getChaptersController } from "./get-chapters-controller";
+import { getChaptersUseCase } from "./get-chapters-usecase";
+
+const projectsRepository = new ProjectsRepository();
+
+const cacheRepository = new CacheRepository();
+
+const GetChaptersUseCase = new getChaptersUseCase(
+  projectsRepository,
+  cacheRepository
+);
+
+const GetChaptersController = new getChaptersController(GetChaptersUseCase);
+
+export { GetChaptersController, GetChaptersUseCase };
