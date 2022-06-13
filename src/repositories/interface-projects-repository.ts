f1cb3bf1921694859bab895eafb784291cad8f82
SDMasterGames <sdmastergames@hotmail.com>
2022-06-13 16:55:11 -0400

    abstrações para obter os capitulos e o id da categoria do projeto

--- src/repositories/interface-projects-repository.ts
+++ src/repositories/interface-projects-repository.ts
@@ -1,9 +1,13 @@
+import { Chapter } from "../entities/chapter";
 import { IProjectTags, Project } from "../entities/project";
 import { ProjectsError } from "./errors";
 
 export interface IProjectsRepository {
-  getProjectsReleases(page?: number): Promise<Project[] | ProjectsError>;
   getTagsByIds(id: string[]): Promise<IProjectTags[]>;
-
   getMediaById(id: string): Promise<string>;
+
+  getProjectsReleases(page?: number): Promise<Project[] | ProjectsError>;
+
+  getChaptersByProjectSlug(slug: string): Promise<Chapter[] | ProjectsError>;
+  getCategoryIdByProjectSlug(slug: string): Promise<number | ProjectsError>;
 }
