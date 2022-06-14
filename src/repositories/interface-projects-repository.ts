dcf771fce1b3e0cc86c726e717f8d7a6df2b5aa9
SDMasterGames <sdmastergames@hotmail.com>
2022-06-14 19:54:37 -0400

    mudando a tipagem dos parametros recebidos

--- src/repositories/interface-projects-repository.ts
+++ src/repositories/interface-projects-repository.ts
@@ -3,11 +3,11 @@ import { IProjectTags, Project } from "../entities/project";
 import { ProjectsError } from "./errors";
 
 export interface IProjectsRepository {
-  getTagsByIds(id: string[]): Promise<IProjectTags[]>;
-  getMediaById(id: string): Promise<string>;
+  getTagsByIds(id: number[]): Promise<IProjectTags[]>;
+  getMediaById(id: number): Promise<string>;
 
   getProjectsReleases(page?: number): Promise<Project[] | ProjectsError>;
-  getProjectsByIds(id: string[]): Promise<Project[] | ProjectsError>;
+  getProjectsByIds(id: string): Promise<Project[] | ProjectsError>;
 
   getChaptersByProjectSlug(slug: string): Promise<Chapter[] | ProjectsError>;
   getCategoryIdByProjectSlug(slug: string): Promise<number | ProjectsError>;
