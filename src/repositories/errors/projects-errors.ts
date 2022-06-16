d2bd4ff94d121c93c9b0e4c28ce83b7dfb085e98
SDMasterGames <sdmastergames@hotmail.com>
2022-06-15 22:54:34 -0400

    removendo as tipagem de erro

--- src/repositories/errors/projects-errors.ts
+++
@@ -1,6 +0,0 @@
-export class ProjectsError extends Error{
-  constructor(message: string) {
-    super(message);    
-    this.name = "ProjectsError";
-  }
-}
--- src/repositories/interface-projects-repository.ts
+++ src/repositories/interface-projects-repository.ts
@@ -1,17 +1,12 @@
 import { Chapter } from "../entities/chapter";
 import { IProjectTags, Project } from "../entities/project";
-import { ProjectsError } from "./errors";
-
 export interface IProjectsRepository {
   getTagsByIds(id: number[]): Promise<IProjectTags[]>;
   getMediaById(id: number): Promise<string>;
 
-  getProjectsReleases(page?: number): Promise<Project[] | ProjectsError>;
-  getProjectsByIds(
-    id: string,
-    per_page: number
-  ): Promise<Project[] | ProjectsError>;
+  getProjectsReleases(page?: number): Promise<Project[]>;
+  getProjectsByIds(id: string, per_page: number): Promise<Project[]>;
 
-  getChaptersByProjectSlug(slug: string): Promise<Chapter[] | ProjectsError>;
-  getCategoryIdByProjectSlug(slug: string): Promise<number | ProjectsError>;
+  getChaptersByProjectSlug(slug: string): Promise<Chapter[]>;
+  getCategoryIdByProjectSlug(slug: string): Promise<number>;
 }
