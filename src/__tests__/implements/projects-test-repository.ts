485c4717bae20806ce74d63d5a2063501dfbfc10
SDMasterGames <sdmastergames@hotmail.com>
2022-06-15 22:58:17 -0400

    alterando os erros após a remoção da tipagem

--- src/__tests__/implements/projects-test-repository.ts
+++ src/__tests__/implements/projects-test-repository.ts
@@ -1,6 +1,5 @@
 import { Chapter } from "../../entities/chapter";
 import { IProjectTags, Project } from "../../entities/project";
-import { ProjectsError } from "../../repositories/errors";
 import { IProjectsRepository } from "../../repositories/interface-projects-repository";
 
 const projects = [
@@ -13,38 +12,38 @@ const projects = [
 const ChapterFake = Chapter.Fake("test-chapter");
 
 export class ProjectsTestRepository implements IProjectsRepository {
-  getProjectsReleases(page: number = 1): Promise<Project[] | ProjectsError> {
+  getProjectsReleases(page: number = 1): Promise<Project[]> {
     if (page === 1) {
       return Promise.resolve(projects);
     }
     if (page === 2) {
       return Promise.resolve(projects.slice(0, 1));
     }
-    return Promise.resolve(new ProjectsError("No Data"));
+    return Promise.reject(new Error("No Data"));
   }
   getMediaById(id: number): Promise<string> {
     return Promise.resolve("");
   }
-  getCategoryIdByProjectSlug(slug: string): Promise<number | ProjectsError> {
+  getCategoryIdByProjectSlug(slug: string): Promise<number> {
     return Promise.resolve(1);
   }
-  getProjectsByIds(id: string): Promise<Project[] | ProjectsError> {
+  getProjectsByIds(id: string): Promise<Project[]> {
     const ids = id.split(",");
     const projectsid = projects.filter((project) =>
       ids.find((i) => i == String(project.id))
     );
     if (projectsid.length == 0) {
-      return Promise.resolve(new ProjectsError("No Data"));
+      return Promise.reject(new Error("No Data"));
     }
     return Promise.resolve(projectsid);
   }
   getTagsByIds(id: number[]): Promise<IProjectTags[]> {
     return Promise.resolve([]);
   }
-  getChaptersByProjectSlug(slug: string): Promise<ProjectsError | Chapter[]> {
+  getChaptersByProjectSlug(slug: string): Promise<Chapter[]> {
     if (slug === ChapterFake.slug) {
       return Promise.resolve([ChapterFake]);
     }
-    return Promise.resolve(new ProjectsError("No Data"));
+    return Promise.reject(new Error("No Data"));
   }
 }
