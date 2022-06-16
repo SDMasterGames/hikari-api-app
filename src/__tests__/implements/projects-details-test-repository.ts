3ede8dc476d156dd81441296ca9e051a73d84a47
SDMasterGames <sdmastergames@hotmail.com>
2022-06-15 22:00:01 -0400

    implementações para testar os casos de uso

---
+++ src/__tests__/implements/projects-details-test-repository.ts
@@ -0,0 +1,30 @@
+import { ProjectDetail } from "../../entities/project-details";
+import {
+  ICreateData,
+  IProjectsDetailsRepository,
+} from "../../repositories/interface-projects-details-repository";
+
+const ProjectsDetails = new Map<string, ProjectDetail>();
+export class ProjectDetailsTestRepository
+  implements IProjectsDetailsRepository
+{
+  async create(data: ICreateData): Promise<ProjectDetail> {
+    const projectDetail = new ProjectDetail({
+      project_id: data.project_id,
+      project_slug: data.project_slug,
+    });
+    return Promise.resolve(projectDetail);
+  }
+
+  async findById(id: string): Promise<ProjectDetail | null> {
+    return Promise.resolve(ProjectsDetails.get(id) || null);
+  }
+
+  async findBySlug(slug: string): Promise<ProjectDetail | null> {
+    let result = null;
+    ProjectsDetails.forEach((projectDetail) => {
+      if (projectDetail.project_slug === slug) result = projectDetail;
+    });
+    return Promise.resolve(result);
+  }
+}
