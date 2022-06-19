28d294d817bbd6ac1ce6e7828688ebc60d0d1f7b
SDMasterGames <sdmastergames@hotmail.com>
2022-06-19 18:43:02 -0400

    adicionando métodos da abstração para autenticação

---
+++ src/repositories/implements/auth-repository.ts
@@ -0,0 +1,17 @@
+import JWT from "jsonwebtoken";
+import { config } from "../../config/auth";
+import { IAuthRepository } from "../interface-auth-repository";
+
+export class AuthRepository implements IAuthRepository {
+  async authenticate(uuid: string): Promise<string> {
+    const jwt = JWT.sign({ uuid }, config.private_key, {
+      expiresIn: "1m", // 1 minuto
+    });
+    return jwt;
+  }
+
+  async verify(token: string): Promise<any> {
+    const decoded = JWT.verify(token, config.private_key);
+    return decoded;
+  }
+}
