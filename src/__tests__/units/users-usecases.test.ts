2953263e37320159248aba5032611e73d85a630d
SDMasterGames <sdmastergames@hotmail.com>
2022-06-20 16:31:05 -0400

    testes unitários para buscar um usuário usando o id

--- src/__tests__/units/users-usecases.test.ts
+++ src/__tests__/units/users-usecases.test.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from "crypto";
 import { createUserUseCase } from "../../modules/users/create/create-user-usecase";
+import { getUserUseCase } from "../../modules/users/getUser/get-user-usecase";
 import { AuthTestRepository } from "../implements/auth-test-repository";
 import { UsersTestRepository } from "../implements/users-test-repository";
 
@@ -7,18 +8,21 @@ const usersRepository = new UsersTestRepository();
 const authRepository = new AuthTestRepository();
 
 const createUser = new createUserUseCase(usersRepository, authRepository);
+const getUser = new getUserUseCase(usersRepository);
 
 describe("Module - Users", () => {
   const user = {
     email: "test@hotmail.com",
     username: "test",
     uuid: randomUUID(),
+    id: "",
   };
 
   describe("Create User", () => {
     it("deveria criar um usuário com sucesso", async () => {
       const { status, data, error } = await createUser.execute(user);
       expect(status).toBe(201);
+      user.id = data.user.id;
       expect(data.user).toHaveProperty("id");
       expect(data).toHaveProperty("token");
     });
@@ -38,6 +42,20 @@ describe("Module - Users", () => {
       });
       expect(status).toBe(400);
       expect(error.name).toBe("AlreadyExistsError");
+    });
+  });
+
+  describe("Get User", () => {
+    it("deveria retornar um usuário com sucesso", async () => {
+      const { status, data, error } = await getUser.execute({ id: user.id });
+      expect(status).toBe(200);
+      expect(data).toHaveProperty("id");
+    });
+
+    it("não deveria retornar um usuário", async () => {
+      const { status, data, error } = await getUser.execute({ id: "test" });
+      expect(status).toBe(400);
+      expect(error.name).toBe("NotFoundError");
     });
   });
 });
