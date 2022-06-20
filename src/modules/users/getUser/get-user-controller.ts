7efc6fd734124105e432f8a66e42c075cc175e14
SDMasterGames <sdmastergames@hotmail.com>
2022-06-20 16:41:35 -0400

    criação do controller

---
+++ src/modules/users/getUser/get-user-controller.ts
@@ -0,0 +1,12 @@
+import { IHttpRequest, IHttpResponse } from "../../httpHelper";
+import { getUserUseCase } from "./get-user-usecase";
+
+export class getUserController {
+  constructor(private useCase: getUserUseCase) {}
+
+  async handle({ params }: IHttpRequest): Promise<IHttpResponse> {
+    const { id } = params;
+    const result = await this.useCase.execute({ id });
+    return result;
+  }
+}
