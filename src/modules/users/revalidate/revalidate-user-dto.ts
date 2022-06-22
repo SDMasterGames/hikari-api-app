802680785100f54b4bee7c7ba8738f27fd63a60b
SDMasterGames <sdmastergames@hotmail.com>
2022-06-22 01:20:35 -0400

    modulo para revalidar tokens

---
+++ src/modules/users/revalidate/revalidate-user-dto.ts
@@ -0,0 +1,3 @@
+export interface IRevalidateUserRequestDTO {
+  token?: string;
+}
---
+++ src/modules/users/revalidate/revalidate-user-usecase.ts
@@ -0,0 +1,24 @@
+import { IAuthRepository } from "../../../repositories/interface-auth-repository";
+import { MissingParams } from "../../errors";
+import { badRequest, IHttpResponse, ok, serverError } from "../../httpHelper";
+import { IRevalidateUserRequestDTO } from "./revalidate-user-dto";
+
+export class revalidateUserUseCase {
+  constructor(private IAuthRepository: IAuthRepository) {}
+
+  async execute({ token }: IRevalidateUserRequestDTO): Promise<IHttpResponse> {
+    try {
+      if (!token) {
+        return badRequest(new MissingParams("token esta faltando"));
+      }
+
+      const { data, error } = await this.IAuthRepository.verify(token);
+      if (error) {
+        return badRequest(new Error(error.message));
+      }
+      return ok(data);
+    } catch (error: any) {
+      return serverError(error.message);
+    }
+  }
+}
