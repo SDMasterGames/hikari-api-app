dbd26aae825b4896e0908b31a28c68c31eb8b932
SDMasterGames <sdmastergames@hotmail.com>
2022-06-22 01:22:42 -0400

    testes unitários para revalidar tokens

--- src/__tests__/units/users-usecases.test.ts
+++ src/__tests__/units/users-usecases.test.ts
@@ -1,7 +1,10 @@
 import { randomUUID } from "crypto";
+
 import { authUserUseCase } from "../../modules/users/auth/auth-user-usecase";
 import { createUserUseCase } from "../../modules/users/create/create-user-usecase";
 import { getUserUseCase } from "../../modules/users/getUser/get-user-usecase";
+import { revalidateUserUseCase } from "../../modules/users/revalidate/revalidate-user-usecase";
+
 import { AuthTestRepository } from "../implements/auth-test-repository";
 import { UsersTestRepository } from "../implements/users-test-repository";
 
@@ -11,6 +14,7 @@ const authRepository = new AuthTestRepository();
 const createUser = new createUserUseCase(usersRepository, authRepository);
 const authUser = new authUserUseCase(usersRepository, authRepository);
 const getUser = new getUserUseCase(usersRepository);
+const revalidateUser = new revalidateUserUseCase(authRepository);
 
 describe("Module - Users", () => {
   const user = {
@@ -19,7 +23,7 @@ describe("Module - Users", () => {
     uuid: randomUUID(),
     id: "",
   };
-
+  let token = "";
   describe("Create User", () => {
     it("deveria criar um usuário com sucesso", async () => {
       const { status, data, error } = await createUser.execute(user);
@@ -53,7 +57,7 @@ describe("Module - Users", () => {
         email: user.email,
         uuid: user.uuid,
       });
-
+      token = data.token;
       expect(status).toBe(200);
       expect(data).toHaveProperty("token");
       expect(data).toHaveProperty("user");
@@ -61,7 +65,7 @@ describe("Module - Users", () => {
       expect(data.user).toHaveProperty("id");
     });
 
-    it("deveria falhar na ausência do email",async()=>{
+    it("deveria falhar na ausência do email", async () => {
       const { status, data, error } = await authUser.execute({
         email: "",
         uuid: user.uuid,
@@ -69,8 +73,8 @@ describe("Module - Users", () => {
 
       expect(status).toBe(400);
       expect(error.name).toBe("MissingParams");
-    })
-    it("deveria falhar na ausência do uuid",async()=>{
+    });
+    it("deveria falhar na ausência do uuid", async () => {
       const { status, data, error } = await authUser.execute({
         email: user.email,
         uuid: "",
@@ -78,7 +82,32 @@ describe("Module - Users", () => {
 
       expect(status).toBe(400);
       expect(error.name).toBe("MissingParams");
-    })
+    });
+  });
+
+  describe("Revalidate User", () => {
+    it("deveria revalidar o usuário com sucesso", async () => {
+      const { status, data, error } = await revalidateUser.execute({
+        token,
+      });
+      expect(status).toBe(200);
+    });
+
+    it("deveria falha por jwt malformatado", async () => {
+      const { status, data, error } = await revalidateUser.execute({
+        token: "pao",
+      });
+      expect(status).toBe(400);
+      expect(error.message).toBe("jwt malformed");
+    });
+    it("deveria falha por token expirado", async () => {
+      await new Promise((r) => setTimeout(r, 1000));//1 segundo
+      const { status, data, error } = await revalidateUser.execute({
+        token,
+      });
+      expect(status).toBe(400);
+      expect(error.message).toBe("jwt expired");
+    });
   });
 
   describe("Get User", () => {
