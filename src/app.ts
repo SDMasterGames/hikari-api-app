adfc82ecc60ceaff0d427bbc05944ad8a8849926
SDMasterGames <sdmastergames@hotmail.com>
2022-06-22 20:09:18 -0400

    adicionando o endpoint para os detalhes de projetos

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import "dotenv/config";
 import express from "express";
-import { EndpointProjects } from "./routes/";
+import { EndpointProjects, EndpointProjectsDetail } from "./routes/";
 
 export class App {
   public app: express.Application;
   constructor() {
     this.app = express();
+    this.middleware()
     this.initRoutes();
   }
+
+private middleware(){
+  this.app.use(express.json())
+}
 
   private initRoutes() {
     this.app.get("/", (req, res) => {
@@ -15,6 +20,7 @@ export class App {
     });
 
     this.app.use(new EndpointProjects().router);
+    this.app.use(new EndpointProjectsDetail().router);
   }
 
   listen(port: string | number) {
