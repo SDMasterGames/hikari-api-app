ad65921f15fcaf649d9fc50bd62b0b111141877b
SDMasterGames <sdmastergames@hotmail.com>
2022-06-23 16:28:56 -0400

    testes para obter um detalhe de projeto

--- src/__tests__/integration/endpoint-project-detail.test.ts
+++ src/__tests__/integration/endpoint-project-detail.test.ts
@@ -1,14 +1,13 @@
-import {ProjectDetailsRepository} from '../../repositories/implements/projects-details-repository';
+import { ProjectDetailsRepository } from "../../repositories/implements/projects-details-repository";
 import { app } from "../../server";
 import request from "supertest";
 
-const projectDetailRepository = new ProjectDetailsRepository()
+const projectDetailRepository = new ProjectDetailsRepository();
 
 describe("Controller - Project Details", () => {
-
-  afterAll(async()=>{
-    await projectDetailRepository.cleandb()
-  })
+  afterAll(async () => {
+    await projectDetailRepository.cleandb();
+  });
 
   describe("Create - Project Details", () => {
     it("deveria criar o detalhe de um projeto", async () => {
@@ -17,40 +16,68 @@ describe("Controller - Project Details", () => {
         .send({
           project_id: "test_id",
           project_slug: "test_slug",
-        })
+        });
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty("id");
     });
 
-    it("deveria falhar caso já exista",async()=>{
-      const response = await request(app.app).post("/v1/projects/details").send({
-        project_id:"test_id",
-        project_slug:"test_slug"
-      })
-      expect(response.status).toBe(400)
-      expect(response.body).toHaveProperty("error.name","AlreadyExistsError")
-    });
-
-
-    it("deveria falhar na ausência de algum parametro", async () => {
-      const test1 = await request(app.app)
+    it("deveria falhar caso já exista", async () => {
+      const response = await request(app.app)
         .post("/v1/projects/details")
         .send({
-          project_id: "",
+          project_id: "test_id",
           project_slug: "test_slug",
-        })
-      expect(test1.status).toBe(400);
-      expect(test1.body).toHaveProperty("error.name","MissingParams");
+        });
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty("error.name", "AlreadyExistsError");
+    });
 
-      const test2 = await request(app.app)
-      .post("/v1/projects/details")
-      .send({
+    it("deveria falhar na ausência de algum parametro", async () => {
+      const test1 = await request(app.app).post("/v1/projects/details").send({
+        project_id: "",
+        project_slug: "test_slug",
+      });
+      expect(test1.status).toBe(400);
+      expect(test1.body).toHaveProperty("error.name", "MissingParams");
+
+      const test2 = await request(app.app).post("/v1/projects/details").send({
         project_id: "test_id",
         project_slug: "",
-      })
-    expect(test2.status).toBe(400);
-    expect(test2.body).toHaveProperty("error.name","MissingParams");
+      });
+      expect(test2.status).toBe(400);
+      expect(test2.body).toHaveProperty("error.name", "MissingParams");
+    });
+  });
 
+  describe("Get - Project Detail", () => {
+    it("deveria obter um detalhe de projeto com sucesso", async () => {
+      const response = await request(app.app)
+        .get("/v1/projects/details")
+        .query({
+          project_id: "test_id",
+          project_slug: "test_slug",
+        });
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("id")
+    });
+    it("deveria falha na ausência de algum item na query", async () => {
+      const test1 = await request(app.app)
+        .get("/v1/projects/details")
+        .query({
+          project_id: "",
+          project_slug: "test_slug",
+        });
+      expect(test1.status).toBe(400);
+      expect(test1.body).toHaveProperty("error.name","MissingParams")
+
+      const test2 = await request(app.app)
+        .get("/v1/projects/details")
+        .query({
+          project_id: "test_id",
+          project_slug: "",
+        });
+      expect(test2.status).toBe(400);
+      expect(test2.body).toHaveProperty("error.name","MissingParams")
     });
   });
 });
