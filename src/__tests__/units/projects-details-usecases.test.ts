13fa24fa2045c8d592208f96b46ab403b46f95f5
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-07 10:18:21 -0400

    mudanças feitas para usar as entiteis novas

--- src/__tests__/units/projects-details-usecases.test.ts
+++ src/__tests__/units/projects-details-usecases.test.ts
@@ -1,12 +1,18 @@
 import { randomUUID } from "crypto";
+import { StatsView } from "../../entities";
 import { createProjectDetailUseCase } from "../../modules/projects-details/createDetail/create-project-detail-usecase";
 import { getProjectDetailUseCase } from "../../modules/projects-details/getDetail/get-project-detail-usecase";
 import { updateProjectDetailUseCase } from "../../modules/projects-details/updateDetail/update-project-detail-usecase";
-import { CacheTestRepository } from "../implements/cache-test-repository";
-import { ProjectDetailsTestRepository } from "../implements/projects-details-test-repository";
+
+import {
+  ProjectDetailsTestRepository,
+  StatsTestRepository,
+  CacheTestRepository,
+} from "../implements/";
 
 const projectDetailsTestRepository = new ProjectDetailsTestRepository();
 const cacheTestRepository = new CacheTestRepository();
+const statsTestRepository = new StatsTestRepository();
 
 const createProjectDetail = new createProjectDetailUseCase(
   projectDetailsTestRepository
@@ -18,7 +24,8 @@ const getProjectDetail = new getProjectDetailUseCase(
 );
 
 const updateProjectDetail = new updateProjectDetailUseCase(
-  projectDetailsTestRepository
+  projectDetailsTestRepository,
+  statsTestRepository
 );
 
 describe("Module - Projects Details", () => {
@@ -35,6 +42,7 @@ describe("Module - Projects Details", () => {
       expect(status).toBe(200);
       expect(data).toHaveProperty("id");
       expect(data).toHaveProperty("project_id");
+      expect(data).toHaveProperty("stats.id");
     });
 
     it("deveria falhar na ausência do project_slug", async () => {
@@ -73,166 +81,100 @@ describe("Module - Projects Details", () => {
     describe("Prop Likes", () => {
       it("deveria atualizar a quantidade de likes adicionando um valor com sucesso", async () => {
         const { status, data, error } = await updateProjectDetail.execute({
-          data: {
-            likes: "test_id",
-          },
+          likes: "test_id",
           id,
         });
         expect(status).toBe(200);
-        expect(data).toHaveProperty("metrics.likes[0]", "test_id");
+        expect(data).toHaveProperty("stats.likes[0]", "test_id");
       });
       it("deveria atualizar a quantidade de likes removendo um valor com sucesso", async () => {
         const { status, data, error } = await updateProjectDetail.execute({
-          data: {
-            likes: "test_id",
-          },
+          likes: "test_id",
           id,
         });
+
         expect(status).toBe(200);
-        expect(data).not.toHaveProperty("metrics.likes[0]", "test_id");
+        expect(data).not.toHaveProperty("stats.likes[0]", "test_id");
       });
 
-      it("deveria falhar se informa um valor invalido", async () => {
+      it("deveria falhar caso o valor seja inválido.", async () => {
         const test1 = await updateProjectDetail.execute({
-          data: {
-            likes: null,
-          },
+          //@ts-ignore
+          likes: null,
           id,
         });
         expect(test1.status).toBe(400);
         expect(test1.error.name).toBe("InvalidParams");
 
         const test2 = await updateProjectDetail.execute({
-          data: {
-            likes: undefined,
-          },
+          likes: undefined,
           id,
         });
         expect(test2.status).toBe(400);
         expect(test2.error.name).toBe("InvalidParams");
 
         const test3 = await updateProjectDetail.execute({
-          data: {
-            likes: "",
-          },
+          likes: "",
           id,
         });
         expect(test3.status).toBe(400);
         expect(test3.error.name).toBe("InvalidParams");
-      });
-    });
-
-    describe("Prop Clicks", () => {
-      it("deveria atualizar a quantidade do contador dos clicks", async () => {
-        const { status, data, error } = await updateProjectDetail.execute({
-          data: {
-            clicks: 1,
-          },
-          id,
-        });
-        expect(status).toBe(200);
-        expect(data).toHaveProperty("metrics.clicks[0].count", 1);
-        expect(data).toHaveProperty(
-          "metrics.clicks[0].date",
-          new Date().toISOString().split("T")[0]
-        );
-      });
-
-      it("deveria falhar ao informa um valor invalido", async () => {
-        const test1 = await updateProjectDetail.execute({
-          data: {
-            clicks: null,
-          },
-          id,
-        });
-        expect(test1.status).toBe(400);
-        expect(test1.error.name).toBe("InvalidParams");
-
-        const test2 = await updateProjectDetail.execute({
-          data: {
-            clicks: undefined,
-          },
-          id,
-        });
-        expect(test2.status).toBe(400);
-        expect(test2.error.name).toBe("InvalidParams");
-
-        const test3 = await updateProjectDetail.execute({
-          data: {
-            clicks: "",
-          },
-          id,
-        });
-        expect(test3.status).toBe(400);
-        expect(test3.error.name).toBe("InvalidParams");
-
-        const test4 = await updateProjectDetail.execute({
-          data: {
-            clicks: "sd",
-          },
-          id,
-        });
-        expect(test4.status).toBe(500);
-        expect(test4.error.name).toBe("ServerError");
       });
     });
 
     describe("Prop Views", () => {
-      it("deveria atualizar a quantidade do contador dos views", async () => {
+      it("deveria adicionar um novo contador de views", async () => {
         const { status, data, error } = await updateProjectDetail.execute({
-          data: {
-            views: 1,
-          },
+          views: "1",
           id,
         });
         expect(status).toBe(200);
-        expect(data).toHaveProperty("metrics.views[0].count", 1);
+        expect(data).toHaveProperty("stats.views[0].count", 0);
         expect(data).toHaveProperty(
-          "metrics.views[0].date",
-          new Date().toISOString().split("T")[0]
+          "stats.views[0].date",
+          StatsView.DateNow()
         );
       });
 
-      it("deveria falhar ao informa um valor invalido", async () => {
+      it("deveria atualizar aumentando o valor do count", async () => {
+        const { status, data, error } = await updateProjectDetail.execute({
+          views: "1",
+          id,
+        });
+        expect(status).toBe(200);
+        expect(data).toHaveProperty("stats.views[0].count", 1);
+        expect(data).toHaveProperty(
+          "stats.views[0].date",
+          StatsView.DateNow()
+        );
+      });
+
+      it("deveria falhar caso seja informado um valor inválido", async () => {
         const test1 = await updateProjectDetail.execute({
-          data: {
-            views: null,
-          },
+          //@ts-ignore
+          views: null,
           id,
         });
         expect(test1.status).toBe(400);
         expect(test1.error.name).toBe("InvalidParams");
 
         const test2 = await updateProjectDetail.execute({
-          data: {
-            views: undefined,
-          },
+          views: undefined,
           id,
         });
         expect(test2.status).toBe(400);
         expect(test2.error.name).toBe("InvalidParams");
 
         const test3 = await updateProjectDetail.execute({
-          data: {
-            views: "",
-          },
+          views: "",
           id,
         });
         expect(test3.status).toBe(400);
         expect(test3.error.name).toBe("InvalidParams");
-
-        const test4 = await updateProjectDetail.execute({
-          data: {
-            views: "sd",
-          },
-          id,
-        });
-        expect(test4.status).toBe(500);
-        expect(test4.error.name).toBe("ServerError");
       });
     });
 
-    describe("Prop Comments", () => {
+    /* describe("Prop Comments", () => {
       it("deveria adicionar um comentario com sucesso", async () => {
         const { status, data, error } = await updateProjectDetail.execute({
           id,
@@ -244,7 +186,6 @@ describe("Module - Projects Details", () => {
         expect(data).toHaveProperty("comments[0]", "test_id");
       });
 
-
       it("deveria falha caso seja informado um id invalid", async () => {
         const { status, data, error } = await updateProjectDetail.execute({
           id,
@@ -255,6 +196,6 @@ describe("Module - Projects Details", () => {
         expect(status).toBe(400);
         expect(error.name).toBe("InvalidParams");
       });
-    });
+    }); */
   });
 });
