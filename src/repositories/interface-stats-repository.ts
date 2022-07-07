6303594d17bf51a46cafbb402b60098590317c81
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-07 10:23:55 -0400

    abstrações

---
+++ src/repositories/interface-stats-repository.ts
@@ -0,0 +1,6 @@
+import { StatsData } from "./ports";
+
+export interface IStatsRepository {
+  addView(StatsId: number): Promise<StatsData>;
+  incrementCountView(StatsId: number, viewId: number): Promise<StatsData>;
+}
