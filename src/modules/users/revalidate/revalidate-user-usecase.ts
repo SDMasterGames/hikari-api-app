a8c3d6bf8e0f6fa0eb5d88bec4939c5468bc90c6
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-10 23:36:01 -0400

    mudando o erro no badRequest

--- src/modules/users/revalidate/revalidate-user-usecase.ts
+++ src/modules/users/revalidate/revalidate-user-usecase.ts
@@ -1,5 +1,5 @@
 import { IAuthRepository } from "../../../repositories/interface-auth-repository";
-import { MissingParams } from "../../errors";
+import { AuthError, MissingParams } from "../../errors";
 import { badRequest, IHttpResponse, ok, serverError } from "../../httpHelper";
 import { IRevalidateUserRequestDTO } from "./revalidate-user-dto";
 
@@ -11,12 +11,11 @@ export class revalidateUserUseCase {
 			if (!token) {
 				return badRequest(new MissingParams("token esta faltando"));
 			}
-
 			const { data, error } = await this.IAuthRepository.verify(token);
 			if (error) {
-				return badRequest(new Error(error.message));
+				return badRequest(new AuthError(error.message));
 			}
-			return ok(data);
+			return ok(undefined);
 		} catch (error: any) {
 			return serverError(error.message);
 		}
